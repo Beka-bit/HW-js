51733305d8e65220b60cd078e89ce981eea97b46
Bekarys <b6134991@gmail.com>
2024-11-06 13:47:38 +0600

    HW_WEEK

---
+++ HW_WEEK10/js/repost.js
@@ -0,0 +1,21 @@
+// Бірінші
+let age = 70
+let isStudent = false
+alert((age>=65) || isStudent)
+
+// Екінші
+
+var num1 = Number(prompt('Write a number: '));
+alert((num1>=10) && (num1<=20))
+//Үшінші
+let num1= -5;
+let num2= 10 
+
+alert((num1 > 0 && num2 < 0) || (num1 < 0 && num2 > 0 ))
+let n=Number(prompt('Сан жазыңыз: '))
+alert(n%2==0)
+if (n%2==0) {
+    alert('Сіздіңің жазған саныңыз ' + n + ' жұп сан')
+}else{
+    alert('Сіздің жазған саныңыз ' + n + ' тақ сан')
+}
