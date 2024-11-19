530e3c73a5da3a2373b8c4f32cdbdd154318f020
Bekarys <b6134991@gmail.com>
2024-11-19 22:33:47 +0600

    HW-W12-D2

--- HW-W12-D1/js/style.js
+++ HW-W12-D1/js/style.js
@@ -1,11 +1,10 @@
 let jazy1 = prompt("Soz engiz")
 let jazy2 = prompt("Soz engiz")
 
-function soz(jazy1,jazy2){
-    if( jazy1.toLowerCase() === jazy2jazy2.toLowerCase()){
+function soz(jazy1, jazy2) {
+    if (jazy1.toLowerCase() === jazy2jazy2.toLowerCase()) {
         return "sozder birdei"
-    }else{
+    } else {
         return "sozder artyrli"
     }
 }
-alert(soz(jazy1,jazy2)) 
